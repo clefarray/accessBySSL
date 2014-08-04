df8bcb7ff390338090638f8f5f757ab2448f3457
yama <yamamoto@kyms.jp>
2014-08-04 15:17:28 +0900

    Small fix

--- plugin.accessBySSL.php
+++ plugin.accessBySSL.php
@@ -6,12 +6,10 @@
  * プラグイン設定: &ids=書き換えるドキュメントID(コンマ区切り);int;1 &append=追加する文字列;text;https://;
  */
 
-include_once $modx->config['base_path'] . "assets/plugins/accessBySSL/accessBySSL.inc.php";
-$e = &$modx->Event;
+if($modx->event->name !== 'OnWebPagePrerender')
+	return;
 
-switch($e->name) {
-    case "OnWebPagePrerender":
-        $t = new accessBySSL($ids, $append);
-        $t->process();
-        break;
-}
+include_once $modx->config['base_path'] . "assets/plugins/accessBySSL/accessBySSL.inc.php";
+
+$t = new accessBySSL($ids, $append);
+$t->process();
